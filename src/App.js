efa9e98067c86f6cb6787de9b713bf9846dfc5b7
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-01-03 18:06:35 +0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  const clickBtn = () => {
+    alert('you Clicked motherFucker');
+  };
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <button onClick={clickBtn}>hello motherFucker</button>
     </div>
   );
 }
