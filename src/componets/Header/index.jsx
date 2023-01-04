333d7834b59f1b28a1a5866ab261e65212daaff2
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-01-04 19:45:39 +0500

    1 file modified, 1 added

--- src/componets/Header/index.jsx
+++ src/componets/Header/index.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React from 'react';
+import './header.css';
 
 function Header() {
   return (
-    <div>
-      <h1>Click on it!</h1>
-      <button>Click</button>
-    </div>
-  )
+    <header classname="header">
+      <nav className="nav container">
+        <a href="#" className="nav__logo">
+          Smith
+        </a>
+        <div className="nav__menu">
+          <ul className="nav_list grid">
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
