fe224225a0794ce0fd504077c5c7fff74d81fb24
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-01-09 21:46:47 +0500

    Info-js-completed

--- src/componets/About/Info.jsx
+++ src/componets/About/Info.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 function Info() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="bx bx-award about__icon"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">4+ months of working</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">1 real Project</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-support about__icon"></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
     </div>
   );
 }
