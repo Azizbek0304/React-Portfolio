239d56e227a9b229a97894c812597dde10550bdf
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-01-30 20:56:21 +0500

    1 file modified

--- src/componets/About/Info.jsx
+++ src/componets/About/Info.jsx
@@ -10,13 +10,13 @@ function Info() {
       </div>
 
       <div className="about__box">
-        <i class="bx bx-briefcase-alt about__icon"></i>
+        <i className="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
         <span className="about__subtitle">1 real Project</span>
       </div>
 
       <div className="about__box">
-        <i class="bx bx-support about__icon"></i>
+        <i className="bx bx-support about__icon"></i>
         <h3 className="about__title">Support</h3>
         <span className="about__subtitle">Online 24/7</span>
       </div>
