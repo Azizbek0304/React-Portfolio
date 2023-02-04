246ec90f9bc209a0bbdc8c8507243fd99c2cf2ec
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-02-04 18:21:13 +0500

    footerJsx-is-done

--- src/componets/About/Info.jsx
+++ src/componets/About/Info.jsx
@@ -4,7 +4,7 @@ function Info() {
   return (
     <div className="about__info grid">
       <div className="about__box">
-        <i class="bx bx-award about__icon"></i>
+        <i className="bx bx-award about__icon"></i>
         <h3 className="about__title">Experience</h3>
         <span className="about__subtitle">4+ months of working</span>
       </div>
---
+++ src/componets/Footer/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Abdu</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a href="" className="footer__social-link" target="_blank">
+            <i className="uil uil-instagram"></i>
+          </a>
+          <a href="" className="footer__social-link" target="_blank">
+            <i className="uil uil-linkedin-alt"></i>
+          </a>
+          <a
+            href="https://github.com/Azizbek0304/React-Portfolio"
+            className="fooer__social-link"
+            target="_blank"
+          >
+            <i className="uil uil-github-alt"></i>
+          </a>
+        </div>
+
+        <span className="footer__copy">
+          Frond-end(React) Developer. All rights reserved
+        </span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
