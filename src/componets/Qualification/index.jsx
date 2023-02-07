2b9f72ffbae90e91aa60d3b2aef3ad593288d516
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-02-07 19:41:18 +0500

    2 files modified

--- src/componets/Qualification/index.jsx
+++ src/componets/Qualification/index.jsx
@@ -7,7 +7,7 @@ function Qualification() {
     setToggleState(index);
   };
   return (
-    <section className="qualification section">
+    <section className="qualification section" id="portfolio">
       <h2 className="section__title">Qualification</h2>
       <span className="section__subtitle">My Personal Journey</span>
 
