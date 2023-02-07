5a0ba9b3cf78b3a2062e133536a6f095aff69bb4
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-02-07 16:48:21 +0500

    2 files modified

--- src/componets/ScrollUp/index.jsx
+++ src/componets/ScrollUp/index.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import './scrollup.css';
 
 function ScrollUp() {
+  window.addEventListener('scroll', function () {
+    const scrollup = document.querySelector('.scrollup');
+    this.scrollY >= 560
+      ? scrollup.classList.add('show-scroll')
+      : scrollup.classList.remove('show-scroll');
+  });
   return (
-    <a href="" className="scrollup">
+    <a href="#home" className="scrollup">
       <i className="uil uil-arrow-up scrollup__icon"></i>
     </a>
   );
